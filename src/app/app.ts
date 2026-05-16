bb8037c16d23473f64222b362916357a53fa01d3
Abhay Yadav <140330980+AbhayYadavhbtu@users.noreply.github.com>
2026-05-17 01:03:05 +0530

    abhay-dev 02

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('github-actions');
+  protected readonly title = sinal('github-actions');
 }
